2eb661580bb3fff6b5a6ec2be48435c68e1cd43e
dingledangledongle <105861890+yanchn-lim@users.noreply.github.com>
2024-03-15 04:22:10 +0800

    Camera follow player

---
+++ EldritchEclipse/Assets/Script/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+    [SerializeField]
+    float height;
+    private void Update()
+    {
+        transform.position = player.position + (Vector3.up * height);
+
+    }
+}

09efe716d597cf7d624362301a9ff99929302fec
Thom Low <thomthomlow@gmail.com>
2024-03-22 21:20:59 +0800

    Init

---
+++ EldritchEclipse/Assets/Enemy/BaseEnemy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public partial class BaseEnemy : MonoBehaviour
+    {
+        public int hello;
+    }
+
+
+    public partial class BaseEnemy
+    {
+        public int hello2;
+    }
+
+    //how it will work
+    //coroutine to inflict the damage on the enemy.
+
+}

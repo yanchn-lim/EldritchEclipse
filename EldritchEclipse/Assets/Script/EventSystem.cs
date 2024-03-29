da3f177f0fd79000ce17d681d88b994cc08a91ed
dingledangledongle <105861890+yanchn-lim@users.noreply.github.com>
2024-03-30 01:24:45 +0800

    cookin?

--- EldritchEclipse/Assets/Script/EventSystem.cs
+++ EldritchEclipse/Assets/Script/EventSystem.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,28 +16,84 @@ namespace YC
             {
                 if (instance == null)
                 {
-                    instance = new ();
+                    instance = new();
                 }
                 return instance;
             }
         }
-
+        string DebugFormat = "EVENT SYSTEM : ";
 
         Dictionary<string, EventBase> EventList = new();
 
+        // adding / removing
         public void AddEvent(string name)
         {
+            if (EventList.ContainsKey(name))
+            {
+                Debug.LogError($"{DebugFormat}EVENT ALREADY IN LIST!");
+                return;
+            }
+
             if (!EventList.TryAdd(name, new(name)))
-                Debug.LogError("EVENT SYSTEM : ERROR ADDING EVENT");               
+                Debug.LogError($"{DebugFormat}ERROR ADDING EVENT!");               
         }
 
         public void RemoveEvent(string name)
         {
+            if (!EventList.ContainsKey(name))
+            {
+                Debug.LogError($"{DebugFormat}EVENT NOT IN LIST!");
+                return;
+            }
+
             if (!EventList.Remove(name))
-                Debug.LogError("EVENT SYSTEM : ERROR REMOVING EVENT");
+                Debug.LogError($"{DebugFormat}ERROR REMOVING EVENT!");
         }
 
-        //do event handling here
+
+        //event handling
+        public EventReturn TriggetEvent(string name)
+        {
+            var evnt = EventList[name];
+            var data = evnt.Execute();
+
+            return data;
+        }
+
+        public EventReturn TriggetEvent<T>(string name, EventArgs args)
+        {
+            var evnt = EventList[name];
+            var returnData = evnt.Execute();
+
+            return returnData;
+        }
+    }
+
+    public struct EventArgs
+    {
+        public string Name { get; private set; }
+        public Hashtable Payload;
+
+        public void AddArgs(string name, object data)
+        {
+            Payload.Add(name, data);
+        }
+
+        public EventArgs(string name)
+        {
+            Name = name;
+            Payload = new();
+        }
+    }
+
+    public struct EventReturn
+    {
+        public Hashtable Payload;
+
+        public void AddData(string name, object data)
+        {
+            Payload.Add(name,data);
+        }
     }
 
     //Delegate, Action,Func
@@ -45,9 +102,19 @@ namespace YC
         public string Name { get; private set; }
         Queue<Delegate> _listeners;
 
-        public void Execute()
+        public EventReturn Execute()
         {
             Debug.Log($"EVENT SYSTEM : EXECUTING \"EVENT_{Name}\"");
+
+            //do check if event requires data
+
+            //if Action, ask for args
+
+            //if Func, ask for args + returns
+
+            //if Predicate, ask for bool
+
+            return default;
         }
 
         public EventBase(string name)

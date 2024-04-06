298fda42216fd3b5eb751b402b5a6e3ed6f3fc9f
dingledangledongle <105861890+yanchn-lim@users.noreply.github.com>
2024-04-06 15:11:41 +0800

    ko

--- EldritchEclipse/Assets/Script/Player/XP/XpOrb.cs
+++ EldritchEclipse/Assets/Script/Player/XP/XpOrb.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class XpOrb : MonoBehaviour
 {
 
+    public float xpValue = 5;
+
     public void FlyToPlayer(Transform player)
     {
         StartCoroutine(FlyTowards(player));
@@ -16,8 +18,19 @@ public class XpOrb : MonoBehaviour
         {
             Vector3 dir = player.position - transform.position;
             dir.Normalize();
-            transform.position += dir * 2f * Time.fixedDeltaTime;
+            transform.position += dir * 1f * Time.fixedDeltaTime;
+            
             yield return null;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerStats s = other.GetComponentInParent<PlayerStats>();
+            s.GainXP(5);
+            Destroy(gameObject);
         }
     }
 }
